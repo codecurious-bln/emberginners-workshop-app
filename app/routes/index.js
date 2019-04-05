c652a21e284530b240231993c863a7ca93786bf4
Jessica Jordan <jessica.jordan.fe.dev@gmail.com>
2019-04-05 15:10:00 +0200

    Exercise 3c: STEP - START

---
+++ app/routes/index.js
@@ -0,0 +1,8 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  beforeModel() {
+    /* TODO: re-enable in a later exercise */
+    // this.replaceWith('rentals');
+  }
+});
