677b970403e647f9de9597fa230127db78a38124
Jessica Jordan <jessica.jordan.fe.dev@gmail.com>
2019-04-05 15:14:13 +0200

    Exercise 3c: STEP - COMPLETE

--- app/routes/rentals.js
+++ app/routes/rentals.js
@@ -1,7 +1,18 @@
 import Route from '@ember/routing/route';
+import EmberObject from '@ember/object';
 
 export default Route.extend({
   model() {
-    /* REPLACE ME */
+    let listing = EmberObject.create({
+      id: 'grand-old-mansion',
+      title: 'Grand Old Mansion',
+      owner: 'Veruca Salt',
+      city: 'San Francisco',
+      category: 'Estate',
+      bedrooms: 15,
+      image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
+      description: 'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.'
+    });
+    return listing;
   },
 });
