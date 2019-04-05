fb57b4e36e6a10e37a32e2eaff6a47110159fd0e
Jessica Jordan <jessica.jordan.fe.dev@gmail.com>
2019-04-05 15:26:17 +0200

    Exercise 3d: STEP - COMPLETE

--- app/routes/rentals.js
+++ app/routes/rentals.js
@@ -7,12 +7,12 @@ export default Route.extend({
       id: 'grand-old-mansion',
       title: 'Grand Old Mansion',
       owner: 'Veruca Salt',
-      city: 'San Francisco',
+      city: 'Berlin',
       category: 'Estate',
       bedrooms: 15,
       image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
       description: 'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.',
-      /* REPLACE ME */
+      availabilityStatus: 'for sale',
     });
     return listing;
   },
