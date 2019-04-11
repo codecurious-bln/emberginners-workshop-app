c620dde4f88fedcfbf62dc50c55928cc6e203a41
Jessica Jordan <jessica.jordan.fe.dev@gmail.com>
2019-04-11 16:11:22 +0200

    Exercise 7a: STEP - START

--- app/routes/rentals.js
+++ app/routes/rentals.js
@@ -1,19 +1,7 @@
 import Route from '@ember/routing/route';
-import EmberObject from '@ember/object';
 
 export default Route.extend({
   model() {
-    let listing = EmberObject.create({
-      id: 'grand-old-mansion',
-      title: 'Grand Old Mansion',
-      owner: 'Veruca Salt',
-      city: 'Berlin',
-      category: 'Estate',
-      bedrooms: 15,
-      image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
-      description: 'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.',
-      availabilityStatus: 'for sale',
-    });
-    return listing;
+    return this.store.findAll('rental').then(rentals => rentals.firstObject);
   },
 });
