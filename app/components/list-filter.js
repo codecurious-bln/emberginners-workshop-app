246ddeb149b6e9e90f913d135b3dd8f293c7133c
Jessica Jordan <jessica.jordan.fe.dev@gmail.com>
2019-04-23 15:08:37 +0200

    Exercise 8c: STEP - START

--- app/components/list-filter.js
+++ app/components/list-filter.js
@@ -3,8 +3,21 @@ import { computed } from '@ember/object';
 
 export default Component.extend({
   classNames: ['list-filter'],
-  value: 'all',
+  value: '',
   filterLabel: computed('value', function() {
     return `You have selected: ${this.value}`;
   }),
+
+  init() {
+    this._super(...arguments);
+    this.filter('').then((results) => this.set('results', results));
+  },
+
+  actions: {
+    handleFilterEntry() {
+      let filterInputValue = this.value;
+      let filterAction = this.filter;
+      filterAction(filterInputValue).then((filterResults) => this.set('results', filterResults));
+    }
+  },
 });
