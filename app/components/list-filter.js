ced12db27d734023dcc4a3af124ee48c26db20f8
Jessica Jordan <jessica.jordan.fe.dev@gmail.com>
2019-04-23 12:02:57 +0200

    Exercise 7b: STEP - START

--- app/components/list-filter.js
+++ app/components/list-filter.js
@@ -1,4 +1,10 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+  classNames: ['list-filter'],
+  value: 'all',
+  filterLabel: computed(/* REPLACE ME *//*, */ function() {
+    return `You have selected: ${this.valur}`;
+  }),
 });
